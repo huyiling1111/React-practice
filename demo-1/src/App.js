8d5008db4bfd992e3d6302bcdfb1520974c47f3b
huyiling1111 <1052859577@qq.com>
2020-10-28 12:43:55 +0800

    第一次练习写React 有bug运行不出来，后续再改

---
+++ demo-1/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+const App = () => {
+    return (<div>App组件
+        <Component numbers={[1, 2, 3]} />
+    </div>)
+}
+const Component = (props) => {
+    const array = []
+    for (let i = 0; i < props.numbers.length; i++) {
+        array.push(<div>{i}{
+            props.numbers[i]
+        }</div>)
+    }
+    return <div>{array}</div>
+
+}
